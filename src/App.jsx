e9ab1a5bc98a68a3405f110d2620c0a71cfa91af
Baktybek Tazhibaev <baktybektazhibaev@gmail.com>
2022-10-12 06:26:01 -1000

    Commented out imports to resolve eslint warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Product from "./pages/Product";
 import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
+// import Product from "./pages/Product";
+// import ProductList from "./pages/ProductList";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       {/* <Product /> */}
       {/* <Register /> */}
       {/* <Login /> */}
-      <Cart />
+      {/* <Cart /> */}
     </>
   );
 }
